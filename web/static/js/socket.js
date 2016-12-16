cf299e6a91072bedaada885065388e03048abb35
Edgar Tinajero <cetinajero@gmail.com>
2016-12-15 17:16:05 -0700

    Changes maded in course

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,7 +54,10 @@ let socket = new Socket("/socket")
 socket.connect({token: window.userToken})
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("rooms:lobby", {})
+channel.on('ping', payload => {
+  consolelog(payload)
+})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
